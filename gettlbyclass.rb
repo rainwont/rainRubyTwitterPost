a60d1773eca75644fa0211fbdedf3be219fc0969
rainwont <rainwont@gmail.com>
2015-04-13 07:25:30 +0900

    全部クラスに入れた。
    confirm 認証
    gettl      TL取得
    viewtl    TL表示
    yamlの使い方がまだよくわかってないため、ファイルにkeyを書く必要あり

--- gettlbyclass.rb
+++ gettlbyclass.rb
@@ -1,32 +1,61 @@
 require 'rubygems'
 require 'twitter'
-require 'yaml'
+require 'pp'
 
-config = YAML.load_file 'config.yml'
+
+API_KEY             = 'xxxx'
+API_SECRET          = 'xxxx'
+ACCESS_TOKEN        = 'xxxx'
+ACCESS_TOKEN_SECRET = 'xxxx'
+
+
+class Timeline
+
+# confirm 認証
+  def confirm
+    @client = Twitter::REST::Client.new do |keys|
+      keys.consumer_key        = API_KEY
+      keys.consumer_secret     = API_SECRET
+      keys.access_token        = ACCESS_TOKEN
+      keys.access_token_secret = ACCESS_TOKEN_SECRET
+    end
+  end
+
+# gettl TLの取得
+  def gettl
+    @user_name        = []
+    @user_screen_name = []
+    @text             = []
+
+    i = 0
+    @client.home_timeline.each do |status|
+      @user_name[i]         = "#{status.user.name}"
+      @user_screen_name[i]  = "#{status.user.screen_name}"
+      @text[i]              = "#{status.text}"
+      i += 1
+    end
+  end
+
+# viewtl TLの表示
+  def viewtl
+    @user_name.reverse!
+    @user_screen_name.reverse!
+    @text.reverse!
+
+    (0..19).each do |j|
+      puts "#{@user_name[j]} @#{@user_screen_name[j]}"
+      puts @text[j]
+      puts '----------------------------------------------'
+    end
+  end
+
+end
+
 
 #証明を検証しない
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-$client = Twitter::REST::Client.new do |keys|
-  keys.consumer_key        = config["twitter"]["consumer_key"]
-  keys.consumer_secret     = config["twitter"]["consumer_secret"]
-  keys.access_token        = config["twitter"]["access_token"]
-  keys.access_token_secret = config["twitter"]["access_token_secret"]
-end
-
-class Timeline
-
-  def get_name_text
-    timeline = $client.home_timeline   # タイムライン取得
-      timeline.each do |status|
-        user_name =   "#{status.user.name}"
-        text      =   "#{status.text}"
-        print "#{user_name}\t"
-        puts text
-        puts '--------------------------------------'
-      end
-  end
-end
-
-rainwont_client = Timeline.new
-rainwont_client.get_name_text
+tc = Timeline.new
+tc.confirm        #認証
+tc.gettl          #TL取得
+tc.viewtl         #TL表示
